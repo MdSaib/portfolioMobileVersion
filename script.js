e69b3aca44a7c8f26ba24cfdf2bfb97f70abda90
Md Saib <mdsaib45@gmail.com>
2022-02-01 19:00:40 +0600

    Add js function

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let test = document.querySelector('.menuIcon');
 const openMenu = document.querySelector('#nav');
+let test3 = document.querySelector('.closeIcon');
 
 function test2() {
   openMenu.style.display = 'flex';
+  test3.style.display = 'flex';
 };
 
 test.addEventListener('click', test2);
 
-let test3 = document.querySelector('.closeIcon');
-
 function test4() {
   openMenu.style.display = 'none';
+  test3.style.display = 'none';
 };
 test3.addEventListener('click', test4);
+
